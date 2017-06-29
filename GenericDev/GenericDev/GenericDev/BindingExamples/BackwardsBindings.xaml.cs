4b20fd41d4ff2148c296fd8be19d89905f596a73
jluzio <joao.luzio@gmail.com>
2017-06-29 19:59:49 +0100

    BindingExamples

---
+++ GenericDev/GenericDev/GenericDev/BindingExamples/BackwardsBindings.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GenericDev.BindingExamples
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BackwardsBindings : ContentPage
+	{
+		public BackwardsBindings ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
