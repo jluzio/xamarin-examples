d7fb308e684aed5e47b0106e14fe57e0edc0b4e6
jluzio <joao.luzio@gmail.com>
2017-07-05 18:37:58 +0100

    Adding loading example (WIP)

---
+++ GenericDev/GenericDev/GenericDev/ImageExamples/ExamplesPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GenericDev.ImageExamples
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExamplesPage : ContentPage
+    {
+        public ExamplesPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void OnExercise1ButtonClick(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Exercise1());
+        }
+
+    }
+}
---
+++ GenericDev/GenericDev/GenericDev/ImageExamples/Loading.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GenericDev.ImageExamples
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Loading : ContentPage
+    {
+        public Loading()
+        {
+            InitializeComponent();
+
+            var imageSource = (UriImageSource) ImageSource.FromUri(new Uri("todo-uri"));
+            imageSource.CachingEnabled = false;
+
+            image.Source = imageSource;
+        }
+    }
+}
