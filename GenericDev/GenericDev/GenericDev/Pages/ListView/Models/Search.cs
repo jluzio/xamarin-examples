ef3bfd0514bdeb1686bd0f6cb67d2f0b1adeba14
jluzio <joao.luzio@gmail.com>
2017-07-10 19:37:10 +0100

    ListView Exercise 1

---
+++ GenericDev/GenericDev/GenericDev/Pages/ListView/Models/Search.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDev.ListView.Models
+{
+    class Search
+    {
+        public int Id { get; set; }
+        public String Location { get; set; }
+        public DateTime CheckIn{ get; set; }
+        public DateTime CheckOut { get; set; }
+    }
+}
---
+++ GenericDev/GenericDev/GenericDev/Pages/ListView/Services/ISearchService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GenericDev.ListView.Models;
+
+namespace GenericDev.ListView.Services
+{
+    interface ISearchService
+    {
+        IEnumerable<Search> GetSearches(String filter = null);
+        void DeleteSearch(int searchId);
+    }
+}
