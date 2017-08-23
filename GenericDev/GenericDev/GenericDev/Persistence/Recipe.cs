8c975f74f1b53af4694e798902512c94c6e31cf4
jluzio <joao.luzio@gmail.com>
2017-08-23 18:08:38 +0100

    Recipe SQLite example (WIP)

---
+++ GenericDev/GenericDev/GenericDev/Persistence/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDev.Persistence
+{
+    public class Recipe
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public String Name { get; set; }
+
+        [MaxLength(250)]
+        public String Description { get; set; }
+    }
+}
--- GenericDev/GenericDev/GenericDev/Views/DataAccessVw/SQLiteExamplePage.xaml.cs
+++ GenericDev/GenericDev/GenericDev/Views/DataAccessVw/SQLiteExamplePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using GenericDev.Persistence;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,54 @@ namespace GenericDev.Views.DataAccessVw
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SQLiteExamplePage : ContentPage
     {
+        private RecipeRepository recipeRepository = new RecipeRepository();
+        private ObservableCollection<Recipe> recipes;
+
         public SQLiteExamplePage()
         {
             InitializeComponent();
+        }
 
-            var tester = new DatabaseTest();
-            tester.Test();
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await recipeRepository.Setup();
+            LoadRecipes();
+        }
+
+        private async void LoadRecipes()
+        {
+            var recipesLoad = await recipeRepository.List();
+            recipes = new ObservableCollection<Recipe>(recipesLoad);
+            recipeList.ItemsSource = recipes;
+        }
+
+        private async void addBtn_Clicked(object sender, EventArgs e)
+        {
+            var recipe = new Recipe { Name = "Recipe " + DateTime.Now.Ticks };
+            await recipeRepository.Insert(recipe);
+            LoadRecipes();
+        }
+
+        private async void updateBtn_Clicked(object sender, EventArgs e)
+        {
+            var recipe = recipeList.SelectedItem as Recipe;
+            if (recipe != null)
+            {
+                await recipeRepository.InsertOrReplace(recipe);
+                LoadRecipes();
+            }
+        }
+
+        private async void removeBtn_Clicked(object sender, EventArgs e)
+        {
+            var recipe = recipeList.SelectedItem as Recipe;
+            if (recipe != null)
+            {
+                await recipeRepository.Delete(recipe);
+                LoadRecipes();
+            }
         }
     }
 }
